21dc3707ab4346c41948252e9c680a7003b9057b
Javi <Jjaviergonzalez23@gmail.com>
2025-05-08 13:37:12 +0200

    'photo'

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import foto from '../../public/foto-portfolio.png'
+import foto from '../images/javi.jpg'
 import { Link } from 'react-router-dom';
 
 export const Index = () => {
