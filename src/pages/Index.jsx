61d95a1164e2bb829dd69a77aabb7f85b692f60e
Javi <Jjaviergonzalez23@gmail.com>
2025-06-05 12:57:51 +0200

    'retoques'

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -11,7 +11,6 @@ export const Index = () => {
         <div className='text-container fade-in'>
           <h1 className='text1'>
             Autodidacta por pasión, frontend developer por vocación.
-            Construyendo el futuro línea a línea.
           </h1>
           <p>Hola, mi nombre es Javier González, desarrollador frontend enfocado en React.</p>
           <button className='btn'><Link to='/projects'>Ver proyectos</Link></button>
