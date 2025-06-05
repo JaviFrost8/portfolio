18101a605a1537b753505d94b58cb116ccda6c7b
Javi <Jjaviergonzalez23@gmail.com>
2025-06-05 13:22:45 +0200

    'contact'

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,10 +6,10 @@ export const Contact = () => {
       <div className='contact-text'>
         <h2>¿Hablamos?</h2>
         <p>
-          Estoy buscando una oportunidad para realizar prácticas en el mundo del desarrollo web. Quiero seguir aprendiendo, aportar lo que sé y formar parte de un equipo que me ayude a seguir creciendo.
+          Estoy buscando una oportunidad para realizar prácticas en el mundo del desarrollo web o una oportunidad para mi primer puesto de trabajo como junior. Quiero seguir aprendiendo, aportar lo que sé y formar parte de un equipo que me ayude a seguir creciendo.
         </p>
         <p>
-          Si crees que encajo en tu empresa o proyecto, no dudes en escribirme. Estoy motivado, con muchas ganas de dar lo mejor de mí, y totalmente abierto a nuevos retos.
+          Si crees que encajo en tu empresa o proyecto, dime donde puedo ponerme en contacto. Estoy motivado, con muchas ganas de dar lo mejor de mí, y totalmente abierto a nuevos retos.
         </p>
       </div>
       <div className='form-container'>
