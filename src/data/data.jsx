229ebcd57e7095ea65b5506cf7f857647eb2423b
Javi <Jjaviergonzalez23@gmail.com>
2025-07-08 11:46:22 +0200

    'newProject'

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -3,10 +3,19 @@ import toDo from '../../public/projects/ListaTareas.png'
 import collector from '../../public/projects/Collector.png'
 import commerce from '../../public/projects/E-commerce.png'
 import landing from '../../public/projects/Landing.png'
+import toDoRedux from '../../public/projects/ToDo-Redux.png'
 
 export const projects = [
   {
     id: 0,
+    nombre: 'To-Do App con Redux',
+    descripcion: 'Aplicación de tareas realizadas con React y Redux, añade y borra herramientas y mantenlas filtradas por completadas o pendientes.',
+    imagen: toDoRedux,
+    demo: 'https://todoreduxvercel.vercel.app/',
+    github: 'https://github.com/JaviFrost8/toDo-Redux',
+  },
+  {
+    id: 1,
     nombre: 'E-commerce',
     descripcion: 'Este proyecto es un e-commerce para un negocio local de mi ciudad sobre tratamientos corporales y cuidado personal.',
     imagen: commerce,
@@ -14,7 +23,7 @@ export const projects = [
     github: 'https://github.com/JaviFrost8/stettic',
   },
   {
-    id: 1,
+    id: 2,
     nombre: 'Landing Page',
     descripcion: 'Esta es una landing page desarrollada con React y diseñada para ser clara, moderna y atractiva desde el primer vistazo. Su objetivo es presentar de forma efectiva una marca o producto, guiando al usuario hacia una acción concreta.',
     imagen: landing,
@@ -22,7 +31,7 @@ export const projects = [
     github: 'https://github.com/JaviFrost8/landing-page',
   },
   {
-    id: 2,
+    id: 3,
     nombre: 'The Collector',
     descripcion: 'Aplicación para buscar mediante el consumo de una API películas y añadirlas a tu colección.',
     imagen: collector,
@@ -30,7 +39,7 @@ export const projects = [
     github: 'https://github.com/JaviFrost8/The-Collector',
   },
   {
-    id: 3,
+    id: 4,
     nombre: 'CRUD de notas',
     descripcion: 'Aplicación desarrollada con React que permite crear, leer, actualizar y eliminar notas de forma sencilla e intuitiva. Ideal para organizar ideas, tareas o recordatorios.',
     imagen: crudImg,
@@ -38,7 +47,7 @@ export const projects = [
     github: 'https://github.com/JaviFrost8/CRUD-notas',
   },
   {
-    id: 4,
+    id: 5,
     nombre: 'ToDo List',
     descripcion: 'Aplicación de tareas con React, manejo de estado y filtros personalizados.',
     imagen: toDo,
